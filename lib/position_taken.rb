f9d04320ea4fc6ac0d6bed43c64c9d1951f6da25
Sunitee Mishra <sunitee.mishra@gmail.com>
2020-08-28 15:29:21 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,6 +4,6 @@ index = 0
 def position_taken? (board, index)
 if(board[index] == " " || board[index] == " " || board[index] == nil)
   taken = false
-#else
-  #taken = true
+else
+  taken = true
 end
